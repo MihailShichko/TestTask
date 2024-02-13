a9d3c74c8e432b99f08f662d824e3db11665c4da
MikhailShychko <mihalshichko@gmail.com>
2024-02-13 07:08:12 +0300

    Add project files.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public OrderService(ApplicationDbContext applicationDbContext)
+        {
+            _dbContext = applicationDbContext;
+        }
+
+        public async Task<Order> GetOrder()
+        {
+            return await _dbContext.Orders.OrderByDescending(order => order.Price * order.Quantity)
+                .FirstAsync();
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            return await _dbContext.Orders.Where(order => order.Quantity > 10)
+                .ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public UserService(ApplicationDbContext applicationDbContext)
+        {
+            _dbContext = applicationDbContext;
+        }
+
+        public async Task<User> GetUser()
+        {
+            var userId = _dbContext.Orders
+                .GroupBy(order => order.UserId)
+                .OrderByDescending(group => group.Count())
+                .Select(group => group.Key)
+                .FirstOrDefault();
+
+            return await _dbContext.Users.Where(user => user.Id == userId).FirstAsync();
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            return await _dbContext.Users.Where(user => user.Status == Enums.UserStatus.Inactive)
+                .ToListAsync();
+
+        }
+    }
+}
